28624b7f727a987fb857d891d3b068cc9124a033
Glenn Rowe <yulunga@gmail.com>
2026-03-26 13:56:21 +0000

    Mobile: add ready-to-run Xcode project

---
+++ mobile/ios/RcktScoreMobile/RcktScoreMobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RcktScoreMobile
+//
+//  Created by Glenn Rowe on 26/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile/ios/RcktScoreMobile/RcktScoreMobile/RcktScoreMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  RcktScoreMobileApp.swift
+//  RcktScoreMobile
+//
+//  Created by Glenn Rowe on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RcktScoreMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
