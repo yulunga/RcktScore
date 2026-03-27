6625b0d58eebcb715fdaf6634256f41b424c5498
Glenn Rowe <yulunga@gmail.com>
2026-03-27 16:16:33 +0000

    Inject app container into native iOS root

--- mobile/ios/RcktScoreMobile/RcktScoreMobile/RcktScoreMobileApp.swift
+++ mobile/ios/RcktScoreMobile/RcktScoreMobile/RcktScoreMobileApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct RcktScoreMobileApp: App {
+    @StateObject private var container = AppContainer()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(container)
         }
     }
 }
