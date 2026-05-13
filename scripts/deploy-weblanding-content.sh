c53ba9a09a8d5f9df7213b7c74c7c8bb47a6db7a
Glenn Rowe <yulunga@gmail.com>
2026-05-13 15:05:39 +0100

    landing scripts

--- scripts/deploy-weblanding-content.sh
+++ scripts/deploy-weblanding-content.sh
@@ -20,8 +20,28 @@ fi
 
 aws s3 sync "$SOURCE_DIR/" "s3://$BUCKET_NAME/" \
   --delete \
-  --exclude "*.html" \
+  --exclude "*" \
+  --include "*.png" \
+  --include "*.jpg" \
+  --include "*.jpeg" \
+  --include "*.svg" \
+  --include "*.webp" \
+  --include "*.gif" \
+  --include "*.avif" \
+  --include "*.woff" \
+  --include "*.woff2" \
   --cache-control "public,max-age=31536000,immutable"
+
+aws s3 sync "$SOURCE_DIR/" "s3://$BUCKET_NAME/" \
+  --delete \
+  --exclude "*" \
+  --include "*.css" \
+  --include "*.js" \
+  --include "*.ico" \
+  --include "*.json" \
+  --include "*.txt" \
+  --include "*.xml" \
+  --cache-control "public,max-age=300,must-revalidate"
 
 aws s3 sync "$SOURCE_DIR/" "s3://$BUCKET_NAME/" \
   --delete \
