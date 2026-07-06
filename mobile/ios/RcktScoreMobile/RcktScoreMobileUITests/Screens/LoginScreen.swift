df6f2cae1cb3f271099596ed4b57223c31862b57
Glenn Rowe <yulunga@gmail.com>
2026-07-06 13:22:23 +0100

    ammendedments to test framwork

--- mobile/ios/RcktScoreMobile/RcktScoreMobileUITests/Screens/LoginScreen.swift
+++ mobile/ios/RcktScoreMobile/RcktScoreMobileUITests/Screens/LoginScreen.swift
@@ -40,12 +40,38 @@ struct LoginScreen {
     func login(user: TestUser) {
         verifyLoaded()
 
-        usernameField.tap()
-        usernameField.typeText(user.username)
+        focusAndType(in: usernameField, text: user.username)
 
-        passwordField.tap()
-        passwordField.typeText(user.password)
+        focusAndType(in: passwordField, text: user.password)
 
         signInButton.tap()
+    }
+
+    private func focusAndType(in element: XCUIElement, text: String) {
+        XCTAssertTrue(element.waitForExistence(timeout: 5))
+
+        focusElement(element)
+        element.typeText(text)
+    }
+
+    private func focusElement(_ element: XCUIElement, timeout: TimeInterval = 2) {
+        element.tap()
+        waitForKeyboard(timeout: timeout)
+
+        if !app.keyboards.firstMatch.exists {
+            let coordinate = element.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5))
+            coordinate.tap()
+            waitForKeyboard(timeout: timeout)
+        }
+    }
+
+    private func waitForKeyboard(timeout: TimeInterval = 2) {
+        let deadline = Date().addingTimeInterval(timeout)
+        while Date() < deadline {
+            if app.keyboards.firstMatch.exists {
+                return
+            }
+            RunLoop.current.run(until: Date().addingTimeInterval(0.1))
+        }
     }
 }
